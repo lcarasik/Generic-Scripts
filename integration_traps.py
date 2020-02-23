9ffbaff49e13abf140de209413c684bf6c0546d6
lcarasik <lane.carasik@gmail.com>
2020-02-23 16:34:41 -0500

    Updating the function integration

--- integration_traps.py
+++ integration_traps.py
@@ -1,22 +1,50 @@
+'''
+Title: Integration of functions in python
+Author(s): Lane Carasik
+Purpose: Calculate the integral of functions using python NumPy/SciPy trapz integration tool
+'''
 
+'''
+--------------------------------------------------------------------------------------------
+---------------------------------- Import Modules ------------------------------------------
+--------------------------------------------------------------------------------------------
+'''
 import numpy as np
 import matplotlib.pyplot as plt
 
+'''
+--------------------------------------------------------------------------------------------
+--------------------------------- Define functions -----------------------------------------
+--------------------------------------------------------------------------------------------
+'''
+
+# Define 
 num = 60
 z = np.linspace(-np.pi,np.pi,num)
-fluxpro = np.zeros(num)
-for i in range(0,num):
-    fluxpro[i] = np.cos(z[i])
-
+f = np.zeros(num)
 integral = np.zeros(num)
-for i in range(0,num):
-    integral[i] = np.trapz(fluxpro[0:i+1],z[0:i+1])
 
-print(fluxpro[0:num])
+# Define the function
+for i in range(0,num):
+    f[i] = np.cos(z[i])
+
+for i in range(0,num):
+    integral[i] = np.trapz(f[0:i+1],z[0:i+1])
+
+print(f[0:num])
 print(integral)
 
+'''
+--------------------------------------------------------------------------------------------
+--------------------------- Plot the function and integral ---------------------------------
+--------------------------------------------------------------------------------------------
+'''
+
 plt.figure(1)
-plt.plot(z,fluxpro,'k',label = 'Flux Profile')
+plt.plot(z,f,'k',label = 'Function')
 plt.plot(z,integral,'r',label = 'Integral')
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
 plt.legend()
+plt.grid(True)
 plt.show()
